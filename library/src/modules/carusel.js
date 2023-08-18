024e3ae639af859f4d946b6e10de8ae459fc03c7
Alexei Torchinsky <alextor1005@gmail.com>
2023-08-18 16:06:32 +0300

    feat: add slider movement

--- library/src/modules/carusel.js
+++ library/src/modules/carusel.js
@@ -1,11 +1,28 @@
-const paginationButton = document.querySelector('.pagination__button-container');
-const imageFirst = document.querySelector('.slider__img_first');
-const slider = document.querySelector('.about__slider')
+// const paginationButton = document.querySelector('.pagination__button-container');
+// const imageFirst = document.querySelector('.slider__img_first');
+// const slider = document.querySelector('.about__slider');
 
-export const move = () => {
-    paginationButton.addEventListener('click', ()=> {
-        slider.classList.add('move-left');
-        imageFirst.classList.add('move-left'); 
-    })
 
-}
+const sliderImages = document.querySelector('.slider__images');
+export const arrowLeft = document.querySelector('.arrows-btn_left');
+export const arrowRight = document.querySelector('.arrows-btn_right');
+  
+  let position = 1;
+  let step = 475;
+  export const moveLeft = () => {
+    sliderImages.style.transform = `translateX(-${step * position}px)` ;
+    position += 1;
+    if (position === 5) {
+      arrowLeft.removeEventListener('click', moveLeft)
+    }
+    console.log(position)
+
+  }
+
+  export const  moveRight = () => {
+    sliderImages.style.transform = `translateX(${step * position}px)` ;
+    position -= 1;
+    if (position === -4) {
+      arrowLeft.removeEventListener('click', moveRight)
+    }
+  }
