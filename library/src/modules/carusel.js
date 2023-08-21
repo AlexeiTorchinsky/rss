368cc959eaf2469387b8c97f8401a4d78e8ef2bc
Alexei Torchinsky <alextor1005@gmail.com>
2023-08-21 13:10:43 +0300

    feat: add movement to slider

--- library/src/modules/carusel.js
+++ library/src/modules/carusel.js
@@ -11,22 +11,38 @@ export const arrowRight = document.querySelector('.arrows-btn_right');
 console.log( 'sliderImagesAll.length=', sliderImagesAll.length)
   let position = 0;
   let step = 475;
-  let counter = 0;
+  
+
   export const moveLeft = () => {
     position += step;
     console.log(position)
 
+    if (position > 0) {
+      arrowRight.addEventListener('click',  moveRight);
+    }
+
     sliderImages.style.transform = `translateX(-${position}px)` ;
     if (position >= (sliderImagesAll.length - 1) * step) {
       arrowLeft.removeEventListener('click', moveLeft)
-    }
+    } 
+  
+    
 
   }
 
   export const  moveRight = () => {
-    position = 0;
-    position += step;
-    sliderImages.style.transform = `translateX(${ position}px)` ;
-    // position -= 1;
+    if (position >= 0 || position < (sliderImagesAll.length - 1) * step) {
+      arrowLeft.addEventListener('click',  moveLeft);
+    }
+   
+    position -= step;
+    sliderImages.style.transform = `translateX(-${position}px)` ;
+      if (position <= 0) {
+        arrowRight.removeEventListener('click',  moveRight);
+      }
+    
+      
+   
 
+    console.log(position)
   }
