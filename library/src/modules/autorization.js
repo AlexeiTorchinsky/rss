6c1bdf92181765794e9281b557c4de7f47d900bc
Alexei Torchinsky <alextor1005@gmail.com>
2023-09-03 02:49:49 +0300

    feat: implement login n register buttons

--- library/src/modules/autorization.js
+++ library/src/modules/autorization.js
@@ -1,73 +1,103 @@
-import { burgerIcon, body, headerNavMenu, bodyCover, headerNavList, removeClassName } from './burger';
+import {
+  burgerIcon,
+  body,
+  headerNavMenu,
+  bodyCover,
+  headerNavList,
+  removeClassName,
+} from "./burger";
 
-const profileIcon = document.querySelector('.header__profile-icon');
-const registerButton = document.querySelector('.autorisation-menu__register');
-const loginButton = document.querySelector('.autorisation-menu__login');
-const registerModal = document.querySelector('.modal-register');
-const loginModal = document.querySelector('.modal-login');
-export const autorizationMenu = document.querySelector('.autorisation-menu');
-export const  closeRegister = document.querySelector('.modal__close-btn');
+const profileIcon = document.querySelector(".header__profile-icon");
+const registerButton = document.querySelector(".autorisation-menu__register");
+const loginButton = document.querySelector(".autorisation-menu__login");
+const registerModal = document.querySelector(".modal-register");
+const loginModal = document.querySelector(".modal-login");
+export const autorizationMenu = document.querySelector(".autorisation-menu");
+export const closeRegister = document.querySelector(".modal__close-btn");
+export const closeLogin = document.querySelector('.login-close-btn');
+const registerButton2 = document.querySelector('.register-footer__span2');
+const loginButton2 = document.querySelector('.login-footer__span2');
+const registerButton3 = document.querySelector('.digital-library-cards__button');
+const loginButton3 = document.querySelector('.digital-library-cards__button2');
 
 const closeBurger = () => {
-    burgerIcon.classList.remove('_active');
-    headerNavMenu.classList.remove('_active');
-    headerNavList.classList.remove('_active');
-    body.classList.remove('_locked');
-    bodyCover.classList.remove('_active');
-}
+  burgerIcon.classList.remove("_active");
+  headerNavMenu.classList.remove("_active");
+  headerNavList.classList.remove("_active");
+  body.classList.remove("_locked");
+  bodyCover.classList.remove("_active");
+};
 
+profileIcon.addEventListener("click", () => {
+  if (burgerIcon.classList.contains("_active")) {
+    closeBurger();
+  }
+  autorizationMenu.classList.toggle("_opened");
+});
 
-profileIcon.addEventListener('click', () => {
-    if (burgerIcon.classList.contains('_active')) {
-        closeBurger(); 
-    }
-    autorizationMenu.classList.toggle('_opened');
-})
-
-window.addEventListener('click', (e) => {
-    if (autorizationMenu.classList.contains('_opened') && e.target !== profileIcon && !autorizationMenu.contains(e.target)) {
-        autorizationMenu.classList.remove('_opened');
-    }
+window.addEventListener("click", (e) => {
+  if (
+    autorizationMenu.classList.contains("_opened") &&
+    e.target !== profileIcon &&
+    !autorizationMenu.contains(e.target)
+  ) {
+    autorizationMenu.classList.remove("_opened");
+  }
 });
 
 const openRegisterModal = () => {
-    autorizationMenu.classList.remove('_opened');
-    bodyCover.classList.add('_active', 'body__cover_modal-opened');
-    registerModal.classList.add('modal-register_opened')
-    body.classList.add('_locked');
-    window.addEventListener('click', e => {
-        const target = e.target;
-        if (target.classList.contains('body__cover') ) {
-          removeClassName();
-          registerModal.classList.remove('modal-register_opened')
-        }
-      
-      })
-}
+  autorizationMenu.classList.remove("_opened");
+  bodyCover.classList.add("_active", "body__cover_modal-opened");
+  registerModal.classList.add("modal-register_opened");
+  body.classList.add("_locked");
+  window.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("body__cover")) {
+      removeClassName();
+      registerModal.classList.remove("modal-register_opened");
+    }
+  });
+};
 
 const openLoginModal = () => {
-    autorizationMenu.classList.remove('_opened');
-    bodyCover.classList.add('_active', 'body__cover_modal-opened');
-    loginModal.classList.add('modal-login_opened')
-    body.classList.add('_locked');
-    window.addEventListener('click', e => {
-        const target = e.target;
-        if (target.classList.contains('body__cover') ) {
-          removeClassName();
-          loginModal.classList.remove('modal-login_opened');
-        }
-      
-      })
-}
+  autorizationMenu.classList.remove("_opened");
+  bodyCover.classList.add("_active", "body__cover_modal-opened");
+  loginModal.classList.add("modal-login_opened");
+  body.classList.add("_locked");
+  window.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("body__cover")) {
+      removeClassName();
+      loginModal.classList.remove("modal-login_opened");
+    }
+  });
+};
 
-registerButton.addEventListener('click', () => {
-    openRegisterModal();
+registerButton.addEventListener('click',  openRegisterModal);
+
+registerButton3.addEventListener('click',  openRegisterModal);
+
+closeRegister.addEventListener("click", () => {
+  removeClassName();
+  registerModal.classList.remove("modal-register_opened");
+});
+
+closeLogin.addEventListener("click", () => {
+  removeClassName();
+  loginModal.classList.remove("modal-login_opened");
+});
+
+loginButton.addEventListener("click", openLoginModal);
+loginButton3.addEventListener("click", openLoginModal);
+
+registerButton2.addEventListener('click', () => {
+  removeClassName();
+  registerModal.classList.remove("modal-register_opened");
+  openLoginModal();
 })
 
-closeRegister.addEventListener('click', () => {
-    removeClassName();
-    registerModal.classList.remove('modal-register_opened');
-    loginModal.classList.remove('modal-login_opened');
+loginButton2.addEventListener('click', () => {
+  removeClassName();
+  loginModal.classList.remove("modal-login_opened");
+  openRegisterModal();
 })
-
-loginButton.addEventListener('click',  openLoginModal)
