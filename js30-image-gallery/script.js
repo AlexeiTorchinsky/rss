a5b663e6e37888574cc6fdfb163ee8c106c6891a
Alexei Torchinsky <alextor1005@gmail.com>
2023-09-28 14:54:43 +0300

    feat: add searh

--- js30-image-gallery/script.js
+++ js30-image-gallery/script.js
@@ -1,26 +1,36 @@
 
-const galleryContainer = document.getElementById('gallery-conatiner');
+const clientId = 'ZIVZYc50_EDW342j99ztm9GD8eysuDudzTwr4Bgmi4c';
 
 
 
-const url = 'https://api.unsplash.com/search/photos?query=car&per_page=30&orientation=landscape&client_id=ZIVZYc50_EDW342j99ztm9GD8eysuDudzTwr4Bgmi4c';
-
+let url = `https://api.unsplash.com/search/photos?query=car&per_page=30&orientation=landscape&client_id=${clientId}`;
+const search = document.getElementById('search');
+const searchBtn = document.querySelector('.search-btn');
 
 async function getData() {
 	const res = await fetch(url);
 	const data = await res.json();
 	
+	
+
 	const showData = (data) => {
 			const galleryContainer = document.getElementById('gallery-container');
 
 			data.results.forEach((result, i) => {
-					const img = `<img class="gallery-img" src="${result.urls.regular}" alt="image ${i + 1}">`;
+					const img = `<img class="gallery-img" src="${result.urls.regular}" alt="">`;
 					galleryContainer.insertAdjacentHTML('beforeend', img);
 			});
 	}
 
 	showData(data);
 }
+
+searchBtn.addEventListener('click', () => {
+	const galleryContainer = document.getElementById('gallery-container');
+  url = `https://api.unsplash.com/search/photos?query=${search.value}&per_page=30&orientation=landscape&client_id=ZIVZYc50_EDW342j99ztm9GD8eysuDudzTwr4Bgmi4c`;
+	galleryContainer.innerHTML = '';
+	getData();
+});
 
 getData();
 
