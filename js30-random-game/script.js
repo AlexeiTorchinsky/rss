4e556d7dd21fa270948f987abe45ec13eb952b2c
Alexei Torchinsky <alextor1005@gmail.com>
2023-10-11 11:08:19 +0300

    fix: fix some fumctionality

--- js30-random-game/script.js
+++ js30-random-game/script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const cactus = document.querySelector('.cactus');
 const buttonStart = document.querySelector('.start-button')
 const timerDisplay = document.querySelector('.timer');
+const gameOver = document.querySelector('.game-over');
 
 
 
@@ -29,13 +30,17 @@ let timerInterval;
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
+let savedTime = 0;
 
 const startGame = () => {
   isGame = true;
   buttonStart.textContent = 'Finish';
   cactus.classList.add('cactus-moving');
   dino.classList.add('dino-running');
-
+  if (gameOver.classList.contains('_opnend')) {
+    gameOver.classList.remove('_opened');
+  } 
+  
     const startTime = Date.now();
     timerInterval = setInterval(() => {
       const currentTime = Date.now() - startTime;
@@ -49,13 +54,21 @@ const startGame = () => {
         (milliseconds < 100 ? '0' : '') + (milliseconds < 10 ? '0' : '') + milliseconds;
       timerDisplay.textContent = formattedTime;
     }, 100);
+    
 }
+
+
+
 
 const finishGame = () => {
   buttonStart.textContent = 'Start';
   cactus.classList.remove('cactus-moving');
   dino.classList.remove('dino-running');
   isGame = false;
+  
+  clearInterval(timerInterval);
+  
+
 }
 
 
@@ -69,10 +82,16 @@ buttonStart.addEventListener('click', () => {
 })
 
 
-// let isAlive = setInterval(function(){
-//   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
-//   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
-//   if (cactusLeft < 45 && dinoTop >= 120) {
-//     alert('game over');
-//   }
-// })
+
+let checkStatus = setInterval(function(){
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+  if (cactusLeft < 45 && dinoTop >= 120) {
+    
+    finishGame();
+    dino.style.top = `${dinoTop}px`;
+    cactus.style.left = `${cactusLeft}px`;
+    gameOver.classList.add('_opened');
+    
+  }
+})
